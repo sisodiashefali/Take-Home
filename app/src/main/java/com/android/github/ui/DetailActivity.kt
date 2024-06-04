2717eaed9d0e172f17415a66bb335209f1c71c5e
Shefali <sisodiashefali27@gmail.com>
2024-06-04 06:34:34 -0400

    date format changed

--- app/src/main/java/com/android/github/ui/DetailActivity.kt
+++ app/src/main/java/com/android/github/ui/DetailActivity.kt
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.android.github.databinding.ActivityDetailBinding
 
 import com.android.github.models.RepositoryListItem
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
@@ -24,17 +29,34 @@ class DetailActivity : AppCompatActivity() {
         if (repoListItem != null) {
             binding.textViewName.text = "Name:${repoListItem.name}"
             binding.textViewDescription.text = "Description:${repoListItem.description}"
-            binding.textViewUpdatedAt.text = "Updated At:${repoListItem.updated_at}"
-            binding.textViewStargazersCount.text ="Stargazers Count:${repoListItem.stargazers_count.toString()}"
 
-            if(repoListItem.forks_count>5000){
+            binding.textViewUpdatedAt.text = "Updated At:${repoListItem.updated_at.getDateWithServerTimeStamp()}"
+            binding.textViewStargazersCount.text =
+                "Stargazers Count:${repoListItem.stargazers_count.toString()}"
+
+            if (repoListItem.forks_count > 5000) {
                 binding.textViewForksCount.setTextColor(Color.RED)
-                binding.textViewForksCount.text ="Fork Count:${ repoListItem.forks_count.toString() } *"
-            }else{
+                binding.textViewForksCount.text =
+                    "Fork Count:${repoListItem.forks_count.toString()} *"
+            } else {
                 binding.textViewForksCount.setTextColor(Color.BLACK)
-                binding.textViewForksCount.text ="Fork Count:${ repoListItem.forks_count.toString() }"
+                binding.textViewForksCount.text =
+                    "Fork Count:${repoListItem.forks_count.toString()}"
             }
         }
 
+    }
+
+     fun String.getDateWithServerTimeStamp(): Date? {
+        val dateFormat = SimpleDateFormat(
+            "yyyy-MM-dd'T'HH:mm:ss'Z'",
+            Locale.getDefault()
+        )
+        dateFormat.timeZone = TimeZone.getDefault()
+        try {
+            return dateFormat.parse(this)
+        } catch (e: ParseException) {
+            return null
+        }
     }
 }
